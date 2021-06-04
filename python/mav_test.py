17f58ad3b2bba6ef539a611aa72b29b8afc65709
Aaron Hunter <aa_hunter@yahoo.com>
2021-06-03 17:07:22 -0700

    enhanced: python scripts for collecting and calibrating IMU data

--- python/mav_test.py
+++ python/mav_test.py
@@ -14,6 +14,8 @@ master = mavutil.mavlink_connection("COM5", baud=57600)
 master.wait_heartbeat()
 print("target_system {}, target component {} \n", master.target_system,master.target_component)
 msg = None
+num_points = 2000
+data = np.zeros((num_points,3))
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
@@ -23,19 +25,23 @@ with open('test_data.csv', 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     writer.writeheader()
-    for i in range(1000):
+    for i in range(num_points):
         try:
             msg = master.recv_match(type='RAW_IMU',blocking = True)
             writer.writerow(msg.to_dict())
-            #print(msg.to_dict())
-            if(i % 10 == 0):
-                xs = msg.xacc
-                ys = msg.yacc
-                zs = msg.zacc
-                ax.scatter(xs, ys, zs,color = 'blue')
-                plt.draw()
+            data[i,0] = msg.xacc
+            data[i,1] = msg.yacc
+            data[i,2] = msg.zacc
+            # print(msg.to_dict())
+            # if(i % 10 == 0):
+            #     xs = msg.xacc
+            #     ys = msg.yacc
+            #     zs = msg.zacc
+                # ax.scatter(xs, ys, zs,color = 'blue')
+                # plt.draw()
         except:
             print("msg type exception")
 
-    
+ax.scatter(data[:,0],data[:,1],data[:,2])
+
 

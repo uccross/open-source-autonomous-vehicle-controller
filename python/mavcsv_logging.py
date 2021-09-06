5615a945a83cf1079e891d32d817dae92ded5b6e
Pavlo Vlastos <pvlastos@ucsc.edu>
2021-09-06 13:27:52 -0700

    adding more echo sounder functionality for testing and then improving origanization of similar sensors

--- python/mavcsv_logging.py
+++ python/mavcsv_logging.py
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*-
 import time
 from pymavlink import mavutil
+from brping import Ping1D
 import os
 import csv
 
+###############################################################################
+# Sensors connected to the Raspberry Pi itself (instead of the micro)
 
+# Ping Echo Sounder for depth measurements in water
+myPing = Ping1D()
+myPing.connect_serial("/dev/ttyUSB1", 115200)
+
+if myPing.initialize() is False:
+    print("Failed to initialize Ping!")
+    exit(1)
+
+###############################################################################
 # Create the connection
 # Need to provide the serial port and baudrate
 print("Starting mavcsv_logging.py\n")
-# master = mavutil.mavlink_connection("/dev/ttyUSB0", baud=57600) # usb on Pi
-master = mavutil.mavlink_connection("COM6", baud=115200) # usb on Pi
+master = mavutil.mavlink_connection("/dev/ttyUSB0", baud=115200) # usb on Pi
+# master = mavutil.mavlink_connection("COM6", baud=115200) # usb on Pi
 # find the OSAVC controller
 master.wait_heartbeat(timeout = 10)
 if master.target_system == 0:
@@ -56,7 +68,10 @@ with open(csv_file, 'w', newline='') as csvfile:
         try:
             msg = master.recv_match(blocking = True)
 
+            print("Type:")
             print(type(msg))
+
+            print("\r\nMsg:")
             print(msg)
 
             # add msg to the msgs_dict
@@ -64,7 +79,19 @@ with open(csv_file, 'w', newline='') as csvfile:
             # and write it to the file
             writer.writerow(msgs_dict)
 
-            # echo_msg = master.
+            # Echo depth sounder sensor [Placed here only temporarily]
+            echo_data = myPing.get_distance()
+            echo_distane = echo_data["distane"]
+            echo_confidence = echo_data["confidence"]
+
+            echo_msg = mavutil.mavlink.MAV_DISTANCE_SENSOR_UNKNOWN(
+                [(time.time - start_time)*1000, 0, 3000, ]
+            )
+            print("Type:")
+            print(type(echo_msg))
+
+            print("\r\nMsg:")
+            print(echo_msg)
 
         except:
             time.sleep(1)

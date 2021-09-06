903f88f1a5874e6042a469f7424919e310eb69ed
Pavlo Vlastos <pvlastos@ucsc.edu>
2021-09-06 13:31:47 -0700

    fixing echo message addition before main loop

--- python/mavcsv_logging.py
+++ python/mavcsv_logging.py
@@ -20,14 +20,15 @@ if myPing.initialize() is False:
 # Create the connection
 # Need to provide the serial port and baudrate
 print("Starting mavcsv_logging.py\n")
-master = mavutil.mavlink_connection("/dev/ttyUSB0", baud=115200) # usb on Pi
+master = mavutil.mavlink_connection("/dev/ttyUSB0", baud=115200)  # usb on Pi
 # master = mavutil.mavlink_connection("COM6", baud=115200) # usb on Pi
 # find the OSAVC controller
-master.wait_heartbeat(timeout = 10)
+master.wait_heartbeat(timeout=10)
 if master.target_system == 0:
     print('No system detected!')
 else:
-    print('target_system {}, target component {} \n'.format(master.target_system,master.target_component))
+    print('target_system {}, target component {} \n'.format(
+        master.target_system, master.target_component))
 
 # setup automatic text logging.  NB: can't read these files yet!  TODO:  email forum for help
 # file = '/mnt/usb/logfiles/logfile.log'
@@ -43,30 +44,36 @@ msgs_dict = {}
 # Collect messages for one second to get all keys
 start_time = time.time()
 wait_time = 5
-print("Collecting messages for {} seconds to get all keys".format(wait_time))
+print("Collecting messages from micro for {} seconds to get all keys".format(wait_time))
 while time.time() - start_time < wait_time:
     try:
-        msg = master.recv_match(blocking = True)
+        msg = master.recv_match(blocking=True)
         # add msg to the msgs_dict
         msgs_dict.update(msg.to_dict())
     except:
         print('msg error, or dict error!')
-        
-print("Finished collecting messages")
 
-# Put all  keys for all the incoming messages into the headers list 
+print("Finished collecting messages from micro")
+
+print("Collecting message from echo sounder sensor")
+echo_msg = mavutil.mavlink.MAV_DISTANCE_SENSOR_UNKNOWN(
+    [(time.time - start_time)*1000, 0, 3000, ]
+)
+msgs_dict.update(echo_msg.to_dict())
+
+# Put all  keys for all the incoming messages into the headers list
 headers = list(msgs_dict)
 print(headers)
 
 with open(csv_file, 'w', newline='') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=headers)    
+    writer = csv.DictWriter(csvfile, fieldnames=headers)
     writer.writeheader()
     start_time = time.time()
     logging_time = 30
     # currently we log for a specified period of time
     while time.time() - start_time < logging_time:
         try:
-            msg = master.recv_match(blocking = True)
+            msg = master.recv_match(blocking=True)
 
             print("Type:")
             print(type(msg))
@@ -100,6 +107,3 @@ with open(csv_file, 'w', newline='') as csvfile:
 # finish up:
 master.close()
 print('Exiting datalogging script')
-
-
-

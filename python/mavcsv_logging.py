407073b9ca39c889ca1f18455aff79987f7c2206
Pavlo Vlastos <pvlastos@ucsc.edu>
2021-09-06 12:50:47 -0700

    testing addition of echo sounder as an example of logging sensors that are connected to the pi instead of the micro

--- python/mavcsv_logging.py
+++ python/mavcsv_logging.py
@@ -8,7 +8,8 @@ import csv
 # Create the connection
 # Need to provide the serial port and baudrate
 print("Starting mavcsv_logging.py\n")
-master = mavutil.mavlink_connection("/dev/ttyUSB0", baud=57600) # usb on Pi
+# master = mavutil.mavlink_connection("/dev/ttyUSB0", baud=57600) # usb on Pi
+master = mavutil.mavlink_connection("COM6", baud=115200) # usb on Pi
 # find the OSAVC controller
 master.wait_heartbeat(timeout = 10)
 if master.target_system == 0:
@@ -17,25 +18,30 @@ else:
     print('target_system {}, target component {} \n'.format(master.target_system,master.target_component))
 
 # setup automatic text logging.  NB: can't read these files yet!  TODO:  email forum for help
-file = '/mnt/usb/logfiles/logfile.log'
+# file = '/mnt/usb/logfiles/logfile.log'
+file = './logfile.log'
 # initiate logging
 master.setup_logfile(file)
 
 # but we're going to make our own datalogging routine using CSV module
-csv_file = '/mnt/usb/logfiles/logfile.csv'
+csv_file = './logfile.csv'
 
 # first find all the incoming messages:
 msgs_dict = {}
 # Collect messages for one second to get all keys
 start_time = time.time()
-end_time = 1
-while time.time() - start_time < end_time:
+wait_time = 5
+print("Collecting messages for {} seconds to get all keys".format(wait_time))
+while time.time() - start_time < wait_time:
     try:
         msg = master.recv_match(blocking = True)
         # add msg to the msgs_dict
         msgs_dict.update(msg.to_dict())
     except:
         print('msg error, or dict error!')
+        
+print("Finished collecting messages").
+
 # Put all  keys for all the incoming messages into the headers list 
 headers = list(msgs_dict)
 print(headers)
@@ -49,11 +55,19 @@ with open(csv_file, 'w', newline='') as csvfile:
     while time.time() - start_time < logging_time:
         try:
             msg = master.recv_match(blocking = True)
+
+            print(type(msg))
+            print(msg)
+
             # add msg to the msgs_dict
             msgs_dict.update(msg.to_dict())
             # and write it to the file
             writer.writerow(msgs_dict)
+
+            # echo_msg = master.
+
         except:
+            time.sleep(1)
             print('msg error, or dict error!')
 
 # finish up:

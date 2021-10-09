4379d2ed4b69ae8c23c57b9d6a1df58b6c98e3da
Pavlo Vlastos <pvlastos@ucsc.edu>
2021-10-09 16:04:26 -0700

    added attitude vizualiation

--- python/auto_boat_guide_sys/utilities/AttitudeVisualization.py
+++ python/auto_boat_guide_sys/utilities/AttitudeVisualization.py
@@ -87,7 +87,7 @@ class AttitudeVizualizer():
 
         #######################################################################
         # Setup Dynamic Graphing
-        self.fig = plt.figure(figsize=(12, 4))
+        self.fig = plt.figure(figsize=(12, 8))
 
         gs = gridspec.GridSpec(nrows=4, ncols=6)
 

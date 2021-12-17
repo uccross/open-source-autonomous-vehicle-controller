afe7afe4a22a95a98f36677103bfa8f5f6fcc940
Pavlo Vlastos <pvlastos@ucsc.edu>
2021-12-16 19:23:08 -0800

    updating attitude visualization to work with VNC for cool animation later

--- python/auto_boat_guide_sys/utilities/AttitudeVisualization.py
+++ python/auto_boat_guide_sys/utilities/AttitudeVisualization.py
@@ -420,23 +420,15 @@ class AttitudeVizualizer():
             ###################################################################
             # Draw
 
-            # self.background0 = self.fig.canvas.copy_from_bbox(self.ax0.bbox)
-            # #
-            # #
-            # #
-            # self.background1 = self.fig.canvas.copy_from_bbox(self.ax1.bbox)
-            # self.background2 = self.fig.canvas.copy_from_bbox(self.ax2.bbox)
-            # self.background3 = self.fig.canvas.copy_from_bbox(self.ax3.bbox)
-
             # ax0
             self.fig.canvas.restore_region(self.background0)
+            self.background0 = self.fig.canvas.copy_from_bbox(self.ax0.bbox)
             self.ax0.draw_artist(self.projxz)
             self.ax0.draw_artist(self.projyz)
             self.ax0.draw_artist(self.projzx)
             self.ax0.draw_artist(self.lineAttX)
             self.ax0.draw_artist(self.lineAttY)
             self.ax0.draw_artist(self.lineAttZ)
-            self.background0 = self.fig.canvas.copy_from_bbox(self.ax0.bbox)
 
             self.fig.canvas.blit(self.ax0.bbox)
 
@@ -447,7 +439,6 @@ class AttitudeVizualizer():
             self.ax1.draw_artist(self.points4accelX)
             self.ax1.draw_artist(self.points4accelY)
             self.ax1.draw_artist(self.points4accelZ)
-            self.background1 = self.fig.canvas.copy_from_bbox(self.ax1.bbox)
 
             self.fig.canvas.blit(self.ax1.bbox)
 
@@ -458,7 +449,6 @@ class AttitudeVizualizer():
             self.ax2.draw_artist(self.points5magX)
             self.ax2.draw_artist(self.points5magY)
             self.ax2.draw_artist(self.points5magZ)
-            self.background2 = self.fig.canvas.copy_from_bbox(self.ax2.bbox)
 
             self.fig.canvas.blit(self.ax2.bbox)
 
@@ -469,7 +459,6 @@ class AttitudeVizualizer():
             self.ax3.draw_artist(self.points6gyroX)
             self.ax3.draw_artist(self.points6gyroY)
             self.ax3.draw_artist(self.points6gyroZ)
-            self.background3 = self.fig.canvas.copy_from_bbox(self.ax3.bbox)
 
             self.fig.canvas.blit(self.ax3.bbox)
 

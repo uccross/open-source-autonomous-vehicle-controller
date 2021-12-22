b832ab52c1c89232893564f03f577c3713461ea2
Pavlo Vlastos <pvlastos@ucsc.edu>
2021-12-21 19:53:42 -0800

    Updating HIL

--- python/auto_boat_guide_sys/main_guide_sys.py
+++ python/auto_boat_guide_sys/main_guide_sys.py
@@ -178,7 +178,7 @@ if __name__ == '__main__':
     dt_sim = 0.001  # seconds
     dt_uc = 0.01  # seconds
     dt_log = 0.05  # seconds
-    dt_transmit = 0.5  # seconds
+    dt_transmit = 1.0  # seconds
     dt_graph = 0.005
     dt_hard_write = 5.0  # seconds
 

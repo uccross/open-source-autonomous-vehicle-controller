568c6198b2cc2dcb8ee2685780bfda58e596dc18
Pavlo Vlastos <pvlastos@ucsc.edu>
2021-12-30 17:10:36 -0800

    Updating HIL

--- python/auto_boat_guide_sys/main_guide_sys.py
+++ python/auto_boat_guide_sys/main_guide_sys.py
@@ -616,7 +616,7 @@ if __name__ == '__main__':
         # HEARTBEAT and extra information
         print("Waiting for hearbeat...")
         while True:
-            msg = logger.mav_conn.recv_match()      
+            msg = logger.mav_conn.recv_match(blocking=True)      
             if msg:
                 if msg.get_type() == 'HEARTBEAT':
                     break

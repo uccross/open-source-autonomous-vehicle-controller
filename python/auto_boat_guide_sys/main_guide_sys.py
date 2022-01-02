55d8f06663dec2858c479b80e68837f5493ad437
Pavlo Vlastos <pvlastos@ucsc.edu>
2022-01-02 12:29:55 -0800

    updating HIL

--- python/auto_boat_guide_sys/main_guide_sys.py
+++ python/auto_boat_guide_sys/main_guide_sys.py
@@ -318,6 +318,27 @@ if __name__ == '__main__':
 
         # Timing
         t_new = time.time()
+        
+        wp_prev_lla_copy = copy.deepcopy(wp_prev_lla)
+        vehi_pt_lla_copy = copy.deepcopy(vehi_pt_lla)
+        wp_next_lla_copy = copy.deepcopy(wp_next_lla)
+
+        wp_prev_lla_copy2 = copy.deepcopy(wp_prev_lla)
+        vehi_pt_lla_copy2 = copy.deepcopy(vehi_pt_lla)
+        wp_next_lla_copy2 = copy.deepcopy(wp_next_lla)
+
+        wp_prev_ned = LTP.lla2ned2(wp_prev_lla_copy, wp_ref_lla)
+        vehi_pt_ned = LTP.lla2ned2(vehi_pt_lla_copy, wp_ref_lla)
+        wp_next_ned = LTP.lla2ned2(wp_next_lla_copy, wp_ref_lla)
+
+        wp_prev_en = np.array([[wp_prev_ned[0, 1],
+                                wp_prev_ned[0, 0]]])
+        if not simulation_flag:
+            vehi_pt_en = np.array([[vehi_pt_ned[0, 1],
+                                    vehi_pt_ned[0, 0]]])
+
+        wp_next_en = np.array([[wp_next_ned[0, 1],
+                                wp_next_ned[0, 0]]])
 
         # Read the state of the vehicle
         # Request MAVLINK_MSG_ID_RAW_IMU
@@ -347,7 +368,7 @@ if __name__ == '__main__':
                 #         0.0,  # ymag
                 #         0.0,  # zmag
                 #     )
-                # yaw = x_os[2][0]
+                yaw = x_os[2][0]
 
                 # Send GPS position of vehicle to be echoed back
                 vehi_pt_en[0][0] = x_pm[0][0]
@@ -589,27 +610,6 @@ if __name__ == '__main__':
             # Information
             if (t_new - t_info) >= dt_info:
                 t_info = t_new
-
-                wp_prev_lla_copy = copy.deepcopy(wp_prev_lla)
-                vehi_pt_lla_copy = copy.deepcopy(vehi_pt_lla)
-                wp_next_lla_copy = copy.deepcopy(wp_next_lla)
-
-                wp_prev_lla_copy2 = copy.deepcopy(wp_prev_lla)
-                vehi_pt_lla_copy2 = copy.deepcopy(vehi_pt_lla)
-                wp_next_lla_copy2 = copy.deepcopy(wp_next_lla)
-
-                wp_prev_ned = LTP.lla2ned2(wp_prev_lla_copy, wp_ref_lla)
-                vehi_pt_ned = LTP.lla2ned2(vehi_pt_lla_copy, wp_ref_lla)
-                wp_next_ned = LTP.lla2ned2(wp_next_lla_copy, wp_ref_lla)
-
-                wp_prev_en = np.array([[wp_prev_ned[0, 1],
-                                        wp_prev_ned[0, 0]]])
-                if not simulation_flag:
-                    vehi_pt_en = np.array([[vehi_pt_ned[0, 1],
-                                            vehi_pt_ned[0, 0]]])
-
-                wp_next_en = np.array([[wp_next_ned[0, 1],
-                                        wp_next_ned[0, 0]]])
 
                 if (msg_type == 'HEARTBEAT'):
                     heartbeat_msg = msg.to_dict()

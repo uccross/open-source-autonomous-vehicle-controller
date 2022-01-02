d21eee28096fd79bffceab232500a7f54e01a722
Pavlo Vlastos <pvlastos@ucsc.edu>
2022-01-02 15:37:40 -0800

    updating HIL

--- python/auto_boat_guide_sys/main_guide_sys.py
+++ python/auto_boat_guide_sys/main_guide_sys.py
@@ -309,8 +309,8 @@ if __name__ == '__main__':
                                            orientation_state_vec,
                                            radius,
                                            reference_speed=reference_speed,
-                                           min_angle=-30.0*np.pi/180,
-                                           max_angle=30.0*np.pi/180)
+                                           min_angle=-10.0*np.pi/180,
+                                           max_angle=10.0*np.pi/180)
 
         state = 'SENDING_REF_POINT'
         last_state = state

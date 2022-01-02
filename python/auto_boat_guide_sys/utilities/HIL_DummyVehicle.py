5798b896d27aa6f5d0718ec0fb8806d6c7d30a05
Pavlo Vlastos <pvlastos@ucsc.edu>
2022-01-02 15:15:52 -0800

    updating HIL

--- python/auto_boat_guide_sys/utilities/HIL_DummyVehicle.py
+++ python/auto_boat_guide_sys/utilities/HIL_DummyVehicle.py
@@ -92,7 +92,7 @@ class DualModel():
 
         # Adjust the thrust magnitude within the body frame
         self.F_thrust_body.update(x=self.thrust_mag_cmd*np.sin(-self.tvc_angle),
-                                  y=self.thrust_mag_cmd*np.cos(self.tvc_angle),
+                                  y=-self.thrust_mag_cmd*np.cos(self.tvc_angle),
                                   z=0.0)
 
         # rotate the thrust vector into the inertial frame from the body frame

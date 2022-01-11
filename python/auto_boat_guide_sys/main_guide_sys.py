bc3c0178e066d99962b4fe9aef4bb401d2daa8db
Pavlo Vlastos <pvlastos@ucsc.edu>
2022-01-11 12:34:50 -0800

    updating HIL for GNC boat

--- python/auto_boat_guide_sys/main_guide_sys.py
+++ python/auto_boat_guide_sys/main_guide_sys.py
@@ -189,7 +189,7 @@ if __name__ == '__main__':
     dt_sim = 0.001  # seconds
     dt_uc = 0.01  # seconds
     dt_log = 0.05  # seconds
-    dt_transmit = 0.250  # seconds
+    dt_transmit = 0.500  # seconds
     dt_HIL_transmit = 1.0  # seconds
     dt_graph = 0.5
     dt_hard_write = 5.0  # seconds

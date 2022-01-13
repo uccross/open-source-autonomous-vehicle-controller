8ed000ac6d1362be4c2fd73ef5ab2531482166be
Pavlo Vlastos <pvlastos@ucsc.edu>
2022-01-13 13:59:39 -0800

    updating HIL

--- python/auto_boat_guide_sys/main_guide_sys.py
+++ python/auto_boat_guide_sys/main_guide_sys.py
@@ -482,9 +482,8 @@ if __name__ == '__main__':
                     # wp_ref_lla = np.array([[lat, lon, 0.0]])
                     # wp_ref_lla_copy = copy.deepcopy(wp_ref_lla)
 
-                    # if (t_new - t_transmit) >= dt_transmit:
-                    #     t_transmit = t_new
-                    # logger.send_mav_cmd_nav_waypoint(wp_ref_lat_lon, wp_type=0.0)
+                if (t_new - t_transmit) >= dt_transmit:
+                    t_transmit = t_new
                     prev_or_next_tx = 1.0
                     logger.send_mav_ltp_en_waypoint(wp_prev_en, prev_or_next_tx)
 

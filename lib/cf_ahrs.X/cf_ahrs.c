8a90fb919fc7831738a1af297ac8ce1b360f336d
Pavlo Vlastos <pvlastos@ucsc.edu>
2022-02-04 19:58:37 -0800

    adding the matrix exponential version of the complementary filter out of curiousity of accuracy compared to the quaternion implementation

--- lib/cf_ahrs.X/cf_ahrs.c
+++ lib/cf_ahrs.X/cf_ahrs.c
@@ -17,6 +17,9 @@
  * PRIVATE #DEFINES
  *****************************************************************************/
 #define CF_2_PI (2.0 * M_PI)
+#define NORM_ZERO_THRESHOLD 0.01 /* Arbitrarily chosen */
+#define QUATERNION_OR_DCM_BASED 1 /* 0 for quaternion, 1 for DCM-based */
+
 /******************************************************************************
  * PRIVATE DATATYPES 
  *****************************************************************************/
@@ -35,21 +38,30 @@ float kp_acc = 1.0;
 
 /* Magnetometer-related variables */
 float mag_vi[MSZ] = {0.0}; /* Magnetic field inertial reference vector */
+//float mag_vb_common[MSZ] = {0.0}; /* Mag vector rotated into same frame as acc */
 float mag_magnitude = 1.0;
 float r_mag[MSZ][MSZ];
 float kp_mag = 1.0;
+float ki_gyro = 0.0001;
 
 /* Gyro-related variables */
-float gyro_vb[MSZ] = {0.0}; /* Angular rate measurements. Double check signs */
-float gyro_bias[MSZ] = {0.0};
+float omega_vb[MSZ] = {0.0}; /* Angular rates */
+float omega_bias[MSZ] = {0.0};
 float w_meas_sum[MSZ] = {0.0};
+float b[MSZ] = {0.0};
 float w_meas_i[MSZ] = {0.0};
+float w_meas_acc[MSZ] = {0.0};
+float w_meas_mag[MSZ] = {0.0};
 
 /* Multi-use variables */
 float axis_angle = 0.0;
 float axis_v[MSZ] = {0.0};
 float r_hat[MSZ][MSZ];
 float r_hat_t[MSZ][MSZ];
+float R_plus[MSZ][MSZ];
+float R_minus[MSZ][MSZ];
+//float r_align[MSZ][MSZ]; /* Rotation matrix to align mag frame with acc frame */
+//float r_align_t[MSZ][MSZ]; /* Rotation matrix to align mag frame with acc frame */
 float vi_hat[MSZ] = {0.0};
 float dt = 0.01;
 
@@ -125,6 +137,21 @@ void cf_ahrs_init(float desired_dt, const float exp_gyro_bias[MSZ]) {
             0.0, 1.0, 0.0,
             0.0, 0.0, 1.0,
             r_hat_t);
+
+    lin_alg_set_m(
+            1.0, 0.0, 0.0,
+            0.0, 1.0, 0.0,
+            0.0, 0.0, 1.0,
+            R_plus);
+
+    lin_alg_set_m(
+            1.0, 0.0, 0.0,
+            0.0, 1.0, 0.0,
+            0.0, 0.0, 1.0,
+            R_minus);
+
+    //    lin_alg_gen_dcm_with_angles(0.0, 0.0, M_PI, r_align);
+    //    lin_alg_m_transpose(r_align, r_align_t);
 }
 
 void cf_ahrs_set_dt(float desired_dt) {
@@ -132,9 +159,9 @@ void cf_ahrs_set_dt(float desired_dt) {
 }
 
 void cf_ahrs_set_gyro_biases(const float exp_gyro_bias[MSZ]) {
-    gyro_bias[0] = exp_gyro_bias[0];
-    gyro_bias[1] = exp_gyro_bias[1];
-    gyro_bias[2] = exp_gyro_bias[2];
+    omega_bias[0] = exp_gyro_bias[0];
+    omega_bias[1] = exp_gyro_bias[1];
+    omega_bias[2] = exp_gyro_bias[2];
 }
 
 void cf_ahrs_set_kp_acc(float desired_kp_acc) {
@@ -143,6 +170,26 @@ void cf_ahrs_set_kp_acc(float desired_kp_acc) {
 
 void cf_ahrs_set_kp_mag(float desired_kp_mag) {
     kp_mag = desired_kp_mag;
+}
+
+void cf_ahrs_set_ki_gyro(float desired_ki_gyro) {
+    ki_gyro = desired_ki_gyro;
+}
+
+void cf_ahrs_set_mag_vi(float desired_mag_vi[MSZ]) {
+    mag_vi[0] = desired_mag_vi[0];
+    mag_vi[1] = desired_mag_vi[1];
+    mag_vi[2] = desired_mag_vi[2];
+
+    float mag_vi_norm = lin_alg_v_norm(mag_vi);
+
+    /* Kept here more to highlight the fact that it is important to check if 
+     * the norm of a vector is zero for other use cases. Not really practical 
+     * here, since we know the magnetic field should be non-zero in magnitude 
+     */
+    if (mag_vi_norm != 0.0) {
+        lin_alg_v_scale((1.0 / mag_vi_norm), mag_vi);
+    }
 }
 
 void cf_ahrs_update(float acc_vb[MSZ], float mag_vb[MSZ],
@@ -155,10 +202,6 @@ void cf_ahrs_update(float acc_vb[MSZ], float mag_vb[MSZ],
     if (acc_magnitude != 0.0) {
         lin_alg_v_scale(1.0 / acc_magnitude, acc_vb);
     }
-    //    axis_angle = lin_alg_angle_from_2vecs(acc_vb, g_vi);
-    //
-    //    lin_alg_cross(acc_vb, g_vi, axis_v);
-    //    lin_alg_gen_dcm(axis_angle, axis_v, r_acc);
 
     /*************************************************************************/
     /* Magnetometers */
@@ -168,75 +211,108 @@ void cf_ahrs_update(float acc_vb[MSZ], float mag_vb[MSZ],
     if (mag_magnitude != 0.0) {
         lin_alg_v_scale(1.0 / mag_magnitude, mag_vb);
     }
-    //    axis_angle = lin_alg_angle_from_2vecs(mag_vb, mag_vi);
-    //
-    //    lin_alg_cross(mag_vb, mag_vi, axis_v);
-    //    lin_alg_gen_dcm(axis_angle, axis_v, r_mag);
 
-    /*************************************************************************/
-    lin_alg_set_v(0.0, 0.0, 0.0, w_meas_sum); /* Clear the correction vector */
-    lin_alg_m_transpose(r_hat, r_hat_t);
+    if (QUATERNION_OR_DCM_BASED == 0) {
 
-    /* Feedback with accelerometers */
-    lin_alg_m_v_mult(r_hat_t, g_vi, vi_hat);
-    //    lin_alg_rot_v_q(g_vi, cf_yaw, cf_pitch, cf_roll, vi_hat);
-    lin_alg_cross(acc_vb, vi_hat, w_meas_i);
-    lin_alg_v_scale(kp_acc, w_meas_i); /* Weight the acc contribution */
+        /*************************************************************************/
+        lin_alg_set_v(0.0, 0.0, 0.0, w_meas_sum); /* Clear the correction vector */
+        lin_alg_m_transpose(r_hat, r_hat_t);
 
-    /* For each sensors that provides a reference vector, begin to sum 
-     * up the respective elements of the correction vector */
-    lin_alg_set_v(w_meas_i[0], w_meas_i[1], w_meas_i[2], w_meas_sum);
+        /* Feedback with accelerometers */
+        lin_alg_m_v_mult(r_hat_t, g_vi, vi_hat);
+        lin_alg_cross(acc_vb, vi_hat, w_meas_i);
+        lin_alg_v_scale(kp_acc, w_meas_i); /* Weight the acc contribution */
 
-    /* Feedback with magnetometers */
-    lin_alg_m_v_mult(r_hat_t, mag_vi, vi_hat);
-    //    lin_alg_rot_v_q(mag_vi, cf_yaw, cf_pitch, cf_roll, vi_hat);
-    lin_alg_cross(mag_vb, vi_hat, w_meas_i);
-    lin_alg_v_scale(kp_mag, w_meas_i); /* Weight the mag contribution */
+        /* For each sensors that provides a reference vector, begin to sum 
+         * up the respective elements of the correction vector */
+        lin_alg_set_v(w_meas_i[0], w_meas_i[1], w_meas_i[2], w_meas_sum);
 
-    /* Continue the sum of elements for the correction vector */
-    w_meas_sum[0] += w_meas_i[0];
-    w_meas_sum[1] += w_meas_i[1];
-    w_meas_sum[2] += w_meas_i[2];
+        /* Feedback with magnetometers */
+        //    lin_alg_m_v_mult(r_align, mag_vb, mag_vb_common); /* Align mag frame with acc frame */
+        lin_alg_m_v_mult(r_hat_t, mag_vi, vi_hat);
+        lin_alg_cross(mag_vb, vi_hat, w_meas_i);
+        lin_alg_v_scale(kp_mag, w_meas_i); /* Weight the mag contribution */
 
-    /* Gyros plus the correction vector. Gyros sign is flipped to match
-     * the sign of rotation with the reference vectors */
+        /* Continue the sum of elements for the correction vector */
+        w_meas_sum[0] += w_meas_i[0];
+        w_meas_sum[1] += w_meas_i[1];
+        w_meas_sum[2] += w_meas_i[2];
 
-    q_gyro[0] = 0.0;
-    q_gyro[1] = -gyro_vb[0] - w_meas_sum[0] + gyro_bias[0];
+        /* Estimated gyro bias */
+        b[0] = -ki_gyro * w_meas_sum[0];
+        b[1] = -ki_gyro * w_meas_sum[1];
+        b[2] = -ki_gyro * w_meas_sum[2];
 
-    q_gyro[2] = -gyro_vb[1] - w_meas_sum[1] + gyro_bias[1];
+        /* Gyros plus the correction vector. Gyros sign is flipped to match
+         * the sign of rotation with the reference vectors */
 
-    q_gyro[3] = -gyro_vb[2] - w_meas_sum[2] + gyro_bias[2];
+        q_gyro[0] = 0.0;
+        q_gyro[1] = gyro_vb[0] - b[0] + w_meas_sum[0] + omega_bias[0];
 
-    lin_alg_q_mult(q_est, q_gyro, q_est_dot);
+        q_gyro[2] = gyro_vb[1] - b[1] + w_meas_sum[1] + omega_bias[1];
 
-    lin_alg_scale_q(0.5, q_est_dot);
+        q_gyro[3] = gyro_vb[2] - b[2] + w_meas_sum[2] + omega_bias[2];
 
-    /* Primary integration */
-    q_est[0] = q_est[0] + q_est_dot[0] * dt;
-    q_est[1] = q_est[1] + q_est_dot[1] * dt;
-    q_est[2] = q_est[2] + q_est_dot[2] * dt;
-    q_est[3] = q_est[3] + q_est_dot[3] * dt;
+        lin_alg_q_mult(q_est, q_gyro, q_est_dot);
 
-    q_norm = lin_alg_q_norm(q_est);
-    if (q_norm != 0.0) {
-        q_magnitude = 1.0 / q_norm;
+        lin_alg_scale_q(0.5, q_est_dot);
+
+        /* Primary integration */
+        q_est[0] = q_est[0] + q_est_dot[0] * dt;
+        q_est[1] = q_est[1] + q_est_dot[1] * dt;
+        q_est[2] = q_est[2] + q_est_dot[2] * dt;
+        q_est[3] = q_est[3] + q_est_dot[3] * dt;
+
+        q_norm = lin_alg_q_norm(q_est);
+        if (q_norm != 0.0) {
+            q_magnitude = 1.0 / q_norm;
+        }
+        lin_alg_scale_q(q_magnitude, q_est);
+
+        lin_alg_q2euler_abs(q_est, &cf_yaw, &cf_pitch, &cf_roll);
+
+        *yaw = cf_yaw;
+        *pitch = cf_pitch;
+        *roll = cf_roll;
+
+        lin_alg_q_inv(q_est, q_est_conj);
+        lin_alg_q2euler_abs(q_est_conj,
+                &cf_yaw_conj,
+                &cf_pitch_conj,
+                &cf_roll_conj);
+
+        lin_alg_q2dcm(q_est, r_hat);
+        
+    } else {
+        lin_alg_set_v(0.0, 0.0, 0.0, w_meas_sum); /* Clear the correction vector */
+        
+        /* Subtract long-term gyro biases */
+        omega_vb[0] = gyro_vb[0] - omega_bias[0];
+        omega_vb[1] = gyro_vb[1] - omega_bias[1];
+        omega_vb[2] = gyro_vb[2] - omega_bias[2];
+                
+        /* Calculate rates based on the cross products of the inertial aiding 
+         * vectors */
+        
+        /* Accelerometers */
+        lin_alg_m_v_mult(R_minus, g_vi, vi_hat);
+        lin_alg_cross(acc_vb, vi_hat, w_meas_acc);
+        lin_alg_v_scale(kp_acc, w_meas_acc); /* Weight the mag contribution */
+        
+        /* Magnetometers */
+        lin_alg_m_v_mult(R_minus, mag_vi, vi_hat);
+        lin_alg_cross(mag_vi, vi_hat, w_meas_mag);
+        lin_alg_v_scale(kp_mag, w_meas_mag); /* Weight the mag contribution */
+        
+        /* Feedback */
+        omega_vb[0] = omega_vb[0] + w_meas_acc[0] + w_meas_mag[0];
+        omega_vb[1] = omega_vb[1] + w_meas_acc[1] + w_meas_mag[1];
+        omega_vb[2] = omega_vb[2] + w_meas_acc[2] + w_meas_mag[2];
+        
+        /* Integration using matrix exponential */
+        //lin_alg_skew_sym
+        //lin_alg_R_exp
     }
-    lin_alg_scale_q(q_magnitude, q_est);
-
-    lin_alg_q2euler_abs(q_est, &cf_yaw, &cf_pitch, &cf_roll);
-    
-    *yaw = cf_yaw;
-    *pitch = cf_pitch;
-    *roll = cf_roll;
-
-    lin_alg_q_inv(q_est, q_est_conj);
-    lin_alg_q2euler_abs(q_est_conj,
-            &cf_yaw_conj,
-            &cf_pitch_conj,
-            &cf_roll_conj);
-
-    lin_alg_q2dcm(q_est, r_hat);
 }
 
 /******************************************************************************

594ecfc810f3014a6c1e0d70761382112e71b257
Pranay Mathur <37473401+Matnay@users.noreply.github.com>
2022-08-01 22:46:49 +0530

    minor update

--- path-finding/Landmark_Mapper/landmark_detect_map.py
+++ path-finding/Landmark_Mapper/landmark_detect_map.py
@@ -17,11 +17,11 @@ import utils
 
 from Landmark_Mapper import bbox_to_position
 
-# from pycoral.adapters.common import input_size
-# from pycoral.adapters.detect import get_objects
-# from pycoral.utils.dataset import read_label_file
-# from pycoral.utils.edgetpu import make_interpreter
-# from pycoral.utils.edgetpu import run_inference
+from pycoral.adapters.common import input_size
+from pycoral.adapters.detect import get_objects
+from pycoral.utils.dataset import read_label_file
+from pycoral.utils.edgetpu import make_interpreter
+from pycoral.utils.edgetpu import run_inference
 
 
 class LandmarkDetector:

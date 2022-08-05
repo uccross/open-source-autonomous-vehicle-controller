9446e4f63d472a017a3fb5b90e393d2bf2f894f2
Aaron Hunter <aa_hunter@yahoo.com>
2022-08-05 07:56:16 -0700

    tested on hardware successfully

--- Quadcopter/Controller/Quad_passthrough.X/quad_main.c
+++ Quadcopter/Controller/Quad_passthrough.X/quad_main.c
@@ -34,8 +34,9 @@
 #define RAW 1
 #define SCALED 2
 #define NUM_MOTORS 4
-
-
+#define DT 0.02 //integration constant
+#define MSZ 3 //matrix size
+#define QSZ 4 //quaternion size
 
 /*******************************************************************************
  * VARIABLES                                                                   *
@@ -459,7 +460,7 @@ static int calc_pw(int raw_counts) {
     const int denominator = (RC_RX_MAX_COUNTS - RC_RX_MIN_COUNTS);
     const int numerator = (RC_SERVO_MAX_PULSE - RC_SERVO_MIN_PULSE);
     int pulse_width; //servo output in microseconds
-    
+
     pulse_width = (raw_counts - RC_RX_MID_COUNTS) * numerator; // scale to servo values
     pulse_width = pulse_width / denominator; // divide out counts
     pulse_width = pulse_width + RC_SERVO_CENTER_PULSE; // add in midpoint of servo pulse
@@ -514,10 +515,239 @@ void set_control_output(void) {
         RC_servo_set_pulse(throttle[3], MOTOR_4);
     } else {
         INTOL = FALSE;
-            printf("%d, %d, %d, %d, %d, %d, %d \r\n", throttle_raw, phi_raw, theta_raw, psi_raw, hash, hash_check, INTOL);
+        printf("%d, %d, %d, %d, %d, %d, %d \r\n", throttle_raw, phi_raw, theta_raw, psi_raw, hash, hash_check, INTOL);
     }
 
-//        printf("%d, %d, %d, %d, %d, %d, %d \r\n", roll_cmd, pitch_cmd, yaw_cmd, throttle[0], throttle[1], throttle[2], throttle[3]);
+    //        printf("%d, %d, %d, %d, %d, %d, %d \r\n", roll_cmd, pitch_cmd, yaw_cmd, throttle[0], throttle[1], throttle[2], throttle[3]);
+}
+
+/**
+ * @function quat2euler()
+ * @param q A quaternion
+ * @param euler a vector of euler angles in [psi, theta, roll] order
+ */
+void quat2euler(double q[MSZ], double euler[MSZ]) {
+    double q00 = q[0] * q[0];
+    double q11 = q[1] * q[1];
+    double q22 = q[2] * q[2];
+    double q33 = q[3] * q[3];
+
+    // psi
+    euler[0] = atan2(2.0 * (q[1] * q[2] + q[0] * q[3]), ((q00 + q11 - q22 - q33)));
+    // theta
+    euler[1] = asin(2.0 * (q[0] * q[2] - q[1] * q[3]));
+    // phi
+    euler[2] = atan2(2.0 * (q[2] * q[3] + q[0] * q[1]), q00 - q11 - q22 + q33);
+}
+
+/**
+ * @function lin_alg_q_mult()
+ * Multiply two quaternions together
+ * @param p A quaternion
+ * @param q A quaternion
+ * @param r The resulting quaternion
+ */
+void lin_alg_q_mult(double q[QSZ], double p[QSZ], double r[QSZ]) {
+    r[0] = p[0] * q[0] - p[1] * q[1] - p[2] * q[2] - p[3] * q[3];
+    r[1] = p[1] * q[0] + p[0] * q[1] + p[3] * q[2] - p[2] * q[3];
+    r[2] = p[2] * q[0] - p[3] * q[1] + p[0] * q[2] + p[1] * q[3];
+    r[3] = p[3] * q[0] + p[2] * q[1] - p[1] * q[2] + p[0] * q[3];
+}
+
+/**
+ * @function q_rot_v_q()
+ * Rotate a vector from the inertial frame to the body frame
+ * @param v_i, a 3space vector in the inertial frame
+ * @param q an attitude quaternion
+ * sets v_b to the rotated inertial vector in the body frame
+ */
+void q_rot_v_q(double v_i[MSZ], double q[QSZ], double v_b[MSZ]) {
+    double q_i[QSZ];
+    double q_temp[QSZ];
+    double q_conj[QSZ];
+    double q_b[QSZ]; // container for inertial vector in body frame as pure quaternion
+
+    // calculate conjugate of q 
+    q_conj[0] = q[0];
+    q_conj[1] = -q[1];
+    q_conj[2] = -q[2];
+    q_conj[3] = -q[3];
+
+    //   convert v_i to a pure quaternion --> q_i
+    q_i[0] = 0;
+    q_i[1] = v_i[0];
+    q_i[2] = v_i[1];
+    q_i[3] = v_i[2];
+    //   first quaternion product q_i by q --> q_temp
+    lin_alg_q_mult(q_i, q, q_temp);
+    //   second quaternion product q_conj by q_temp -->q_b
+    lin_alg_q_mult(q_conj, q_temp, q_b);
+    //    set v_b to imaginary part of q_b
+    v_b[0] = q_b[1];
+    v_b[1] = q_b[2];
+    v_b[2] = q_b[3];
+}
+
+void v_copy(double m_in[MSZ], double m_out[MSZ]) {
+    int row;
+    for (row = 0; row < MSZ; row++) {
+        m_out[row] = m_in[row];
+    }
+}
+
+/**
+ * @function lin_alg_cross()
+ * @param u A vector
+ * @param v A vector
+ * @return The cross product (sometimes called the outter product) of u and v
+ */
+void lin_alg_cross(double u[MSZ], double v[MSZ], double w_out[MSZ]) {
+    w_out[0] = u[1] * v[2] - u[2] * v[1];
+    w_out[1] = u[2] * v[0] - u[0] * v[2];
+    w_out[2] = u[0] * v[1] - u[1] * v[0];
+}
+
+/**
+ * @function lin_alg_v_scale()
+ * Scales vector
+ * @param s Scalar to scale vector with
+ * @param v Vector to be scaled
+ */
+void lin_alg_v_scale(double s, double v[MSZ]) {
+    int row;
+    for (row = 0; row < MSZ; row++) {
+        v[row] *= s;
+    }
+}
+
+/**
+ * @function lin_alg_v_v_add()
+ * Add a vector value to a vector
+ * @param v1 Vector to add to another vector
+ * @param v2 Vector to have a vector added to it
+ * @param v_out Vector as sum of two vectors
+ */
+void lin_alg_v_v_add(double v1[MSZ], double v2[MSZ], double v_out[MSZ]) {
+    int row;
+
+    for (row = 0; row < MSZ; row++) {
+        v_out[row] = v1[row] + v2[row];
+    }
+}
+
+/**
+ * @function lin_alg_v_v_sub()
+ * Add a vector value to a vector
+ * @param v1 Vector to subtract to another vector
+ * @param v2 Vector to have a vector subtracted to it
+ * @param v_out Vector as difference of two vectors
+ */
+void lin_alg_v_v_sub(double v1[MSZ], double v2[MSZ], double v_out[MSZ]) {
+    int row;
+
+    for (row = 0; row < MSZ; row++) {
+        v_out[row] = v1[row] - v2[row];
+    }
+}
+
+/**
+ * @function lin_alg_q_norm()
+ * @param q A quaternion
+ * @return The magnitude of the quaternion, q
+ */
+double lin_alg_q_norm(double q[QSZ]) {
+    return ((double) sqrt(q[0] * q[0] + q[1] * q[1] + q[2] * q[2] + q[3] * q[3]));
+}
+
+/**
+ * @function m_norm()
+ * @param M A matrix
+ * @return The magnitude of the M, m_norm
+ */
+double m_norm(double M[MSZ]) {
+    return ((double) sqrt(M[0] * M[0] + M[1] * M[1] + M[2] * M[2]));
+}
+
+void ahrs_update(double q_minus[QSZ], double bias_minus[MSZ], double gyros[MSZ],
+        double mags[MSZ], double accels[MSZ], double mag_i[MSZ], double acc_i[MSZ],
+        double dt, double kp_a, double ki_a, double kp_m, double ki_m,
+        double q_plus[QSZ], double bias_plus[MSZ]) {
+
+    double acc_b[MSZ]; //estimated gravity vector in body frame
+    double mag_b[MSZ]; //estimated magnetic field vector in body frame
+
+    double gyro_cal[MSZ]; // gyros with bias correction
+    double gyro_wfb[MSZ]; // gyro 'rate' after feedback
+    double w_meas_ap[MSZ]; // accelerometer proportion correction rate
+    double w_meas_mp[MSZ]; // magnetometer proportional correction rate
+    double w_meas_ai[MSZ]; // accelerometer integral correction rate
+    double w_meas_mi[MSZ]; // magnetometer integral correction rate
+
+    double gyro_q_wfb[QSZ]; // temporary quaternion to hold feedback term
+    double q_dot[QSZ]; // quaternion derivative
+    double b_dot[MSZ]; // bias vector derivative
+    double q_norm;
+    double acc_n;
+    double mag_n;
+
+    /* normalize inertial measurements */
+    acc_n = m_norm(accels);
+    accels[0] = accels[0] / acc_n;
+    accels[1] = accels[1] / acc_n;
+    accels[2] = accels[2] / acc_n;
+
+    mag_n = m_norm(mags);
+    mags[0] = mags[0] / mag_n;
+    mags[1] = mags[1] / mag_n;
+    mags[2] = mags[2] / mag_n;
+
+    /*Accelerometer attitude calculations */
+    q_rot_v_q(acc_i, q_minus, acc_b); //estimate gravity vector in body frame 
+    lin_alg_cross(accels, acc_b, w_meas_ap); // calculate the accelerometer rate term
+    v_copy(w_meas_ap, w_meas_ai); // make a copy for the integral term
+    lin_alg_v_scale(kp_a, w_meas_ap); // calculate the accelerometer proportional feedback term 
+    lin_alg_v_scale(ki_a, w_meas_ai); // calculate the accelerometer integral feedback term 
+
+    /*Magnetometer attitude calculations*/
+    q_rot_v_q(mag_i, q_minus, mag_b); //estimate magnetic field vector in body frame
+    lin_alg_cross(mags, mag_b, w_meas_mp); // calculate the magnetometer rate term
+    v_copy(w_meas_mp, w_meas_mi); //make a copy for the integral term
+    lin_alg_v_scale(kp_m, w_meas_mp); // calculate the magnetometer proportional feedback term
+    lin_alg_v_scale(ki_m, w_meas_mi); // calculate the magnetometer integral feedback term
+
+    /*Gyro attitude contributions */
+    lin_alg_v_v_sub(gyros, bias_minus, gyro_cal); //correct the gyros with the b_minus vector
+
+    /* calculate total rate term gyro_wfb */
+    lin_alg_v_v_add(w_meas_ap, w_meas_mp, gyro_wfb);
+    lin_alg_v_v_add(gyro_cal, gyro_wfb, gyro_wfb);
+
+    /* convert feedback term to a pure quaternion */
+    gyro_q_wfb[0] = 0;
+    gyro_q_wfb[1] = gyro_wfb[0];
+    gyro_q_wfb[2] = gyro_wfb[1];
+    gyro_q_wfb[3] = gyro_wfb[2];
+
+    /* compute the quaternion derivative q_dot */
+    lin_alg_q_mult(q_minus, gyro_q_wfb, q_dot);
+
+    /* integrate term by term */
+    q_plus[0] = q_minus[0] + 0.5 * q_dot[0] * dt;
+    q_plus[1] = q_minus[1] + 0.5 * q_dot[1] * dt;
+    q_plus[2] = q_minus[2] + 0.5 * q_dot[2] * dt;
+    q_plus[3] = q_minus[3] + 0.5 * q_dot[3] * dt;
+
+    // normalize the quaternion for stability
+    q_norm = lin_alg_q_norm(q_plus);
+    q_plus[0] = q_plus[0] / q_norm;
+    q_plus[1] = q_plus[1] / q_norm;
+    q_plus[2] = q_plus[2] / q_norm;
+    q_plus[3] = q_plus[3] / q_norm;
+
+    // compute the integral of the bias term by term
+    bias_plus[0] = bias_minus[0] - (w_meas_ai[0] + w_meas_mi[0]) * dt;
+    bias_plus[1] = bias_minus[1] - (w_meas_ai[1] + w_meas_mi[1]) * dt;
+    bias_plus[2] = bias_minus[2] - (w_meas_ai[2] + w_meas_mi[2]) * dt;
 }
 
 int main(void) {
@@ -530,6 +760,50 @@ int main(void) {
     int8_t IMU_retry = 5;
     uint32_t IMU_error = 0;
     uint8_t error_report = 50;
+
+    /*filter gains*/
+    double kp_a = 2.5; //accelerometer proportional gain
+    double ki_a = 0.05; // accelerometer integral gain
+    double kp_m = 2.5; // magnetometer proportional gain
+    double ki_m = 0.05; //magnetometer integral gain
+    /*timing and conversion*/
+    const double dt = DT;
+    const double deg2rad = M_PI / 180.0;
+    const double rad2deg = 180.0 / M_PI;
+    /* Calibration matrices and offset vectors */
+    float A_acc[MSZ][MSZ] = {
+        {5.98605657636023e-05, 5.02299172664344e-08, 8.41134559461075e-07},
+        {-2.82167981801537e-08, 6.05938345982234e-05, 6.95665927111956e-07},
+        {4.48326742757725e-08, -3.34771681800715e-07, 5.94633160681115e-05}
+    };
+    float A_mag[MSZ][MSZ] = {
+        {0.00333834334834959, 2.58649731866218e-05, -4.47182534891735e-05},
+        {3.97521279910819e-05, 0.00341838979684877, -7.55578863505947e-06},
+        {-6.49436573527762e-05, 3.05050635014235e-05, 0.00334143925188739}
+    };
+    float b_acc[MSZ] = {0.00591423067694908, 0.0173747801090554, 0.0379428158730668};
+    float b_mag[MSZ] = {0.214140746707571, -1.08116057610690, -0.727337561140470};
+
+    // gravity inertial vector
+    double a_i[MSZ] = {0, 0, 1.0};
+    // Earth's magnetic field inertial vector, normalized 
+    // North 22,680.8 nT	East 5,217.6 nT	Down 41,324.7 nT, value from NOAA
+    // converted into ENU format and normalized:
+    double m_i[MSZ] = {0.110011998753301, 0.478219898291142, -0.871322609031072};
+
+    // Euler angles
+    double euler[MSZ] = {0, 0, 0};
+
+    // attitude quaternions
+    double q_minus[QSZ] = {1, 0, 0, 0};
+    double q_plus[QSZ] = {1, 0, 0, 0};
+    // gyro bias vector
+    double b_minus[MSZ] = {0, 0, 0};
+    double b_plus[MSZ] = {0, 0, 0};
+    /* data arrays */
+    double gyro_cal[MSZ] = {0, 0, 0};
+    double acc_cal[MSZ] = {0, 0, 0};
+    double mag_cal[MSZ] = {0, 0, 0};
 
     //Initialization routines
     Board_init(); //board configuration
@@ -551,6 +825,10 @@ int main(void) {
     }
 
     printf("\r\nMinimal Mavlink application %s, %s \r\n", __DATE__, __TIME__);
+    /* load calibration matrices */
+    IMU_set_mag_cal(A_mag, b_mag);
+    IMU_set_acc_cal(A_acc, b_acc);
+
     cur_time = Sys_timer_get_msec();
     control_start_time = cur_time;
     heartbeat_start_time = cur_time;
@@ -581,6 +859,34 @@ int main(void) {
                     printf("IMU error count %d\r\n", IMU_error);
                 }
             }
+        }
+        if (IMU_is_data_ready() == TRUE) {
+            IMU_get_norm_data(&IMU_scaled);
+
+            acc_cal[0] = (double) IMU_scaled.acc.x;
+            acc_cal[1] = (double) IMU_scaled.acc.y;
+            acc_cal[2] = (double) IMU_scaled.acc.z;
+            mag_cal[0] = (double) IMU_scaled.mag.x;
+            mag_cal[1] = (double) IMU_scaled.mag.y;
+            mag_cal[2] = (double) IMU_scaled.mag.z;
+            /*scale gyro readings into rad/sec */
+            gyro_cal[0] = (double) IMU_scaled.gyro.x * deg2rad;
+            gyro_cal[1] = (double) IMU_scaled.gyro.y * deg2rad;
+            gyro_cal[2] = (double) IMU_scaled.gyro.z * deg2rad;
+            ahrs_update(q_minus, b_minus, gyro_cal, mag_cal, acc_cal, m_i,
+                    a_i, dt, kp_a, ki_a, kp_m, ki_m, q_plus, b_plus);
+            quat2euler(q_plus, euler);
+
+            printf("%+3.1f, %+3.1f, %+3.1f \r\n", euler[0] * rad2deg, euler[1] * rad2deg, euler[2] * rad2deg);
+
+            // update b_minus and q_minus
+            b_minus[0] = b_plus[0];
+            b_minus[1] = b_plus[1];
+            b_minus[2] = b_plus[2];
+            q_minus[0] = q_plus[0];
+            q_minus[1] = q_plus[1];
+            q_minus[2] = q_plus[2];
+            q_minus[3] = q_plus[3];
         }
         //publish heartbeat
         if (cur_time - heartbeat_start_time >= HEARTBEAT_PERIOD) {

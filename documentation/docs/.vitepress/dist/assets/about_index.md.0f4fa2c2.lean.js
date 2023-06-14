5ded3e8cee2fde42974a21ca8b2f3c5c3c986eda
Aniruddha1261 <thakreaniruddha1@gmail.com>
2023-06-14 19:55:28 +0530

    Adding dist

---
+++ documentation/docs/.vitepress/dist/assets/about_index.md.0f4fa2c2.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a}from"./chunks/framework.5c100d06.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"about/index.md","filePath":"about/index.md"}'),o={name:"about/index.md"};function n(r,s,c,d,i,p){return t(),a("div")}const f=e(o,[["render",n]]);export{m as __pageData,f as default};

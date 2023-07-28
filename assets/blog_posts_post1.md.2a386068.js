9665398f4647366c85c11844bdc82078c5fbc3ab
Aniruddha1261 <thakreaniruddha1@gmail.com>
2023-07-28 22:01:28 +0530

    Adding dist folder

---
+++ assets/blog_posts_post1.md.2a386068.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, V as createStaticVNode } from "./chunks/framework.ce59e187.js";
+const __pageData = JSON.parse('{"title":"Setting Up Hardware","description":"","frontmatter":{"date":"2023-06-28T00:00:00.000Z","title":"Setting Up Hardware","Author":"Carlos Espinosa","category":"Tutorial","tags":["Hardware","Connections"]},"headers":[],"relativePath":"blog/posts/post1.md"}');
+const _sfc_main = { name: "blog/posts/post1.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<blockquote><p>Getting Trouble Setting up the Hardware ?</p></blockquote><hr><h2 id="connection-failure" tabindex="-1">Connection Failure? <a class="header-anchor" href="#connection-failure" aria-label="Permalink to &quot;Connection Failure?&quot;">​</a></h2><ul><li>Unplug PICkit from computer</li><li>Quit MPLAB X and reopen</li></ul><h2 id="pickit-configuration" tabindex="-1">PICkit configuration <a class="header-anchor" href="#pickit-configuration" aria-label="Permalink to &quot;PICkit configuration&quot;">​</a></h2><h2 id="voltage-not-detected" tabindex="-1">Voltage not Detected ? <a class="header-anchor" href="#voltage-not-detected" aria-label="Permalink to &quot;Voltage not Detected ?&quot;">​</a></h2><p>J2 ICSP pins are connected incorrectly with the PICkit in-circuit debugger&#39;s pins</p>', 7);
+const _hoisted_8 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_8);
+}
+const post1 = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  post1 as default
+};
---
+++ assets/contribute_website_config_nav.md.459485d6.lean.js
@@ -0,0 +1,25 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, C as createBaseVNode, a as createTextVNode } from "./chunks/framework.ce59e187.js";
+const __pageData = JSON.parse('{"title":"Navbar","description":"","frontmatter":{},"headers":[],"relativePath":"contribute/website/config/nav.md"}');
+const _sfc_main = { name: "contribute/website/config/nav.md" };
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode("h1", {
+  id: "navbar",
+  tabindex: "-1"
+}, [
+  /* @__PURE__ */ createTextVNode("Navbar "),
+  /* @__PURE__ */ createBaseVNode("a", {
+    class: "header-anchor",
+    href: "#navbar",
+    "aria-label": 'Permalink to "Navbar"'
+  }, "​")
+], -1);
+const _hoisted_2 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_2);
+}
+const nav = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  nav as default
+};

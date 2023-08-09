a8979ec87f859650fcf42f2d29d3bcc454cabe8e
Aniruddha1261 <thakreaniruddha1@gmail.com>
2023-08-09 11:58:44 +0530

    Adding Dist

---
+++ documentation/docs/.vitepress/dist/assets/contribute_website_getting-started.md.4388e738.lean.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, V as createStaticVNode } from "./chunks/framework.ce59e187.js";
+const __pageData = JSON.parse('{"title":"Getting Started","description":"","frontmatter":{},"headers":[],"relativePath":"contribute/website/getting-started.md"}');
+const _sfc_main = { name: "contribute/website/getting-started.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 32);
+const _hoisted_33 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_33);
+}
+const gettingStarted = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  gettingStarted as default
+};
---
+++ documentation/docs/.vitepress/dist/assets/guide_FirstProject.md.e8b4f94d.lean.js
@@ -0,0 +1,26 @@
+import { _ as _imports_1$1, a as _imports_2$1, b as _imports_3$1, c as _imports_4$1 } from "./chunks/bytes.bfa4cabc.js";
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, V as createStaticVNode } from "./chunks/framework.ce59e187.js";
+const _imports_0 = "/open-source-autonomous-vehicle-controller/assets/connection.5cc0f7fc.jpg";
+const _imports_1 = "/open-source-autonomous-vehicle-controller/assets/usbSerial.68e93808.png";
+const _imports_2 = "/open-source-autonomous-vehicle-controller/assets/baudRate.734f7257.png";
+const _imports_3 = "/open-source-autonomous-vehicle-controller/assets/openProject.7d915e17.png";
+const _imports_4 = "/open-source-autonomous-vehicle-controller/assets/serial.bf94765a.png";
+const _imports_9 = "/open-source-autonomous-vehicle-controller/assets/ok.55d84c34.jpg";
+const _imports_10 = "/open-source-autonomous-vehicle-controller/assets/CleanBuild.65f4ce2d.png";
+const _imports_11 = "/open-source-autonomous-vehicle-controller/assets/MakeProgram.d9e42909.png";
+const _imports_12 = "/open-source-autonomous-vehicle-controller/assets/outputCoolterm.1894a0b6.png";
+const _imports_13 = "/open-source-autonomous-vehicle-controller/assets/everything.9ad8dd08.png";
+const __pageData = JSON.parse('{"title":"Building Your First Project","description":"","frontmatter":{},"headers":[],"relativePath":"guide/FirstProject.md"}');
+const _sfc_main = { name: "guide/FirstProject.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 19);
+const _hoisted_20 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_20);
+}
+const FirstProject = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  FirstProject as default
+};

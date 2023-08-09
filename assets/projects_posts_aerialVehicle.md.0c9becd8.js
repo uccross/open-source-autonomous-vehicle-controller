f01f2b9c70dd0210a91819749a2e7c92596fae54
Aniruddha1261 <thakreaniruddha1@gmail.com>
2023-08-09 11:58:44 +0530

    Adding Dist

---
+++ assets/projects_posts_aerialVehicle.md.0c9becd8.js
@@ -0,0 +1,43 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, C as createBaseVNode, a as createTextVNode } from "./chunks/framework.ce59e187.js";
+const __pageData = JSON.parse('{"title":"Pushpak","description":"","frontmatter":{"Author":"Carlos Espinosa","Updated":"2023-07-30T00:00:00.000Z","image":"https://www.google.com/url?sa=i&url=https%3A%2F%2Fequinoxsdrones.com%2Fblog%2F10-major-pros-cons-of-unmanned-aerial-vehicle-uav-drones&psig=AOvVaw37C0hWpT5hAqjnifWvEXFr&ust=1690649676767000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKC1nP_usYADFQAAAAAdAAAAABAE"},"headers":[],"relativePath":"projects/posts/aerialVehicle.md"}');
+const _sfc_main = { name: "projects/posts/aerialVehicle.md" };
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode("h1", {
+  id: "pushpak",
+  tabindex: "-1"
+}, [
+  /* @__PURE__ */ createTextVNode("Pushpak "),
+  /* @__PURE__ */ createBaseVNode("a", {
+    class: "header-anchor",
+    href: "#pushpak",
+    "aria-label": 'Permalink to "Pushpak"'
+  }, "​")
+], -1);
+const _hoisted_2 = /* @__PURE__ */ createBaseVNode("blockquote", null, [
+  /* @__PURE__ */ createBaseVNode("p", null, "Quadcopter")
+], -1);
+const _hoisted_3 = /* @__PURE__ */ createBaseVNode("h2", {
+  id: "concept",
+  tabindex: "-1"
+}, [
+  /* @__PURE__ */ createTextVNode("Concept "),
+  /* @__PURE__ */ createBaseVNode("a", {
+    class: "header-anchor",
+    href: "#concept",
+    "aria-label": 'Permalink to "Concept"'
+  }, "​")
+], -1);
+const _hoisted_4 = /* @__PURE__ */ createBaseVNode("p", null, "it works by calculating .........", -1);
+const _hoisted_5 = [
+  _hoisted_1,
+  _hoisted_2,
+  _hoisted_3,
+  _hoisted_4
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_5);
+}
+const aerialVehicle = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  aerialVehicle as default
+};

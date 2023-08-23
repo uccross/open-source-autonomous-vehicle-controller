18f925616440b461dab1f497fd7dc0664294950b
Aniruddha1261 <thakreaniruddha1@gmail.com>
2023-08-23 22:29:34 +0530

    Dist Folder

---
+++ documentation/docs/.vitepress/dist/assets/guide_introduction.md.3b9f8da3.lean.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, V as createStaticVNode } from "./chunks/framework.ce59e187.js";
+const __pageData = JSON.parse('{"title":"Introduction to the OSAVC","description":"","frontmatter":{},"headers":[],"relativePath":"guide/introduction.md"}');
+const _sfc_main = { name: "guide/introduction.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 15);
+const _hoisted_16 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_16);
+}
+const introduction = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  introduction as default
+};

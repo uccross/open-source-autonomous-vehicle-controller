e42291dacaa824db8fd0b6cd85b915da29e0b3a5
Aniruddha1261 <thakreaniruddha1@gmail.com>
2023-08-24 23:46:11 +0530

    Adding Dist Folder

---
+++ assets/projects_posts_dora.md.b2fb4c69.lean.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, V as createStaticVNode } from "./chunks/framework.ce59e187.js";
+const __pageData = JSON.parse('{"title":"Dynamically Operated Reconfigurable Autopilot (D.O.R.A) Project","description":"","frontmatter":{},"headers":[],"relativePath":"projects/posts/dora.md"}');
+const _sfc_main = { name: "projects/posts/dora.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 5);
+const _hoisted_6 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_6);
+}
+const dora = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  dora as default
+};
